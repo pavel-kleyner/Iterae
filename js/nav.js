a511cbd7fa76c4d629d20dd967a1fe173bc492fc
John Franklin <franklify@gmail.com>
2018-02-13 23:56:40 -0600

    Initial webpage setup
    Includes nav bar and light start for practice page

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+function menuIcon() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
