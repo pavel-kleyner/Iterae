20ee6323b9805006f6c17a9bd7814cf7a30fefb5
pavel-kleyner <pavelkleyner@gmail.com>
2018-02-18 17:16:33 -0600

    functionality for home page practice page buttons

--- js/practice.js
+++ js/practice.js
@@ -1,7 +1,33 @@
 var tempo = 90;
+var currSelScale = document.getElementsByClassName("selected")[0];
+
 
 function init() {
 	document.getElementById("bpm").value = tempo.toString();
 };
 
+sc1 = document.getElementById('scale1');
+sc2 = document.getElementById('scale2');
+
+
+function pr(){
+	console.log("sc1 " + sc1.className);
+	console.log("sc2 " + sc2.className);
+	console.log('currSelScale ' + currSelScale.id);
+}
+
 init();
+
+function switchScales(){
+		int = document.getElementById("intermediate-scale-container");
+		beg = document.getElementById("beginner-scale-container");
+		int.style.visibility = "visible";
+		beg.style.visibility = "hidden";
+		
+		console.log('beg ' + beg.visibility);
+		console.log('int ' + int.visibility);
+	};
+
+
+
+// make selected a global var
